5efcd0f76cd0696a4de9ac6bb0410822ede15d35
Titus Woo <titus.woo@me.com>
2016-06-03 17:12:36 -0400

    Integrated SimpleMDE into the stack exchange site with appropriate styling. Adjusted CSS of various other parts of the edit ui as well.

--- app/chrome/content/index.js
+++ app/chrome/content/index.js
@@ -1,13 +1,30 @@
 require('script!simplemde/dist/simplemde.min.js')
 require('simplemde/dist/simplemde.min.css')
+require('./styles.css')
 
 console.info('ENHANCING STACK EXCHANGE')
 
-let oldEditor = document.querySelector('.wmd-container')
+// Remove the awk gap between original editor and live preview
+let gap = document.querySelector('.post-editor .fl')
+gap.parentNode.removeChild(gap)
+
+let oldEditor = document.querySelector('.wmd-container').parentNode
 oldEditor.style.display = 'none'
 let oldEditorTextArea = oldEditor.querySelector('textarea')
 
-let editor = document.createElement('div')
-editor.id = 'better-post-editor'
+let editorContainer = document.createElement('div')
+editorContainer.id = 'better-editor'
 
-oldEditor.parentNode.insertBefore(editor, oldEditor.nextSibling)
+let editor = document.createElement('textarea')
+oldEditor.parentNode.insertBefore(editorContainer, oldEditor.nextSibling)
+editorContainer.appendChild(editor)
+
+const config = {
+  element: editor
+}
+
+let newEditor = new SimpleMDE(config)
+
+newEditor.value('Aloha my friends. \`code in here\`')
+
+newEditor.codemirror.setOption('viewportMargin', Infinity)
