3a8d8642e802205a6b87d21054bbf4538a0e76c1
Titus Woo <titus.woo@me.com>
2016-06-04 00:16:14 -0400

    Got SimpleMDE markdown preview to look exactly like the Stack Exchange one (minus images and stuff)

--- app/chrome/content/index.js
+++ app/chrome/content/index.js
@@ -1,5 +1,7 @@
 require('script!simplemde/dist/simplemde.min.js')
 require('simplemde/dist/simplemde.min.css')
+require('script!highlight.js/lib/highlight.js')
+require('highlight.js/styles/github.css')
 require('./styles.css')
 
 console.info('ENHANCING STACK EXCHANGE')
@@ -7,6 +9,10 @@ console.info('ENHANCING STACK EXCHANGE')
 // Remove the awk gap between original editor and live preview
 let gap = document.querySelector('.post-editor .fl')
 gap.parentNode.removeChild(gap)
+
+// Hide the original markdown previewer:
+let mdPreview = document.querySelector('.wmd-preview')
+mdPreview.style.display = 'none'
 
 // Make a new editor:
 
@@ -22,7 +28,19 @@ oldEditor.parentNode.insertBefore(editorContainer, oldEditor.nextSibling)
 editorContainer.appendChild(editor)
 
 const config = {
-  element: editor
+  element: editor,
+  toolbar: [
+    'bold', 'italic', '|',
+    'link', 'quote', 'code', '|',
+    'ordered-list', 'unordered-list', '|',
+    'preview', 'side-by-side', 'fullscreen'
+  ],
+  tabSize: 4,
+  spellChecker: false,
+  status: false,
+  renderingConfig: {
+    codeSyntaxHighlighting: true
+  }
 }
 
 let newEditor = new SimpleMDE(config)
@@ -30,3 +48,9 @@ let newEditor = new SimpleMDE(config)
 newEditor.value('Aloha my friends. \`code in here\`')
 
 newEditor.codemirror.setOption('viewportMargin', Infinity)
+
+newEditor.value(oldEditorTextArea.value)
+
+newEditor.codemirror.on('change', (instance, changeObj) => {
+  oldEditorTextArea.value = newEditor.value()
+})
