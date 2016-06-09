4e15a83f49857cf801ee4bcba3a85e068392b9e8
Titus Woo <titus.woo@me.com>
2016-06-08 22:51:41 -0400

    Fix for StackApps

--- app/shared/enhancers/enhanceAnswerForm.js
+++ app/shared/enhancers/enhanceAnswerForm.js
@@ -1,5 +1,21 @@
 import enhanceAnswerButton from 'shared/enhancers/enhanceSubmitButton'
 
 export default function() {
+
+  // Hide the "Title:" label
+  const postEditor = document.querySelector('.post-editor')
+
+  if (postEditor) {
+    let item = postEditor.querySelector('.form-item td label[for="title"]')
+    item.parentNode.style.display = 'none'
+  }
+
+  // Remove the padding next to the title input element
+  const title = document.querySelector('#title')
+
+  if (title) {
+    title.parentNode.style.paddingLeft = '0'
+  }
+
   enhanceAnswerButton()
 }
